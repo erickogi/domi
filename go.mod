7c6ea0ffcf49e1ca2864871d622c50614ed64e32
DJ Schleen <djschleen@gmail.com>
2021-02-18 13:13:52 -0700

    Initial go-playground\webhooks implementation

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/devops-kung-fu/domi
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	gopkg.in/go-playground/webhooks.v5 v5.17.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,15 +2,44 @@ package main
 
 import (
 	"fmt"
-	"github.com/devops-kung-fu/domi/routes"
+	"log"
+	"net/http"
+
+	"gopkg.in/go-playground/webhooks.v5/github"
 )
 
-var err error
+const (
+	path = "/webhooks/github"
+)
 
 func main() {
-	fmt.Println("domi - Policy-as-Code Enforcer")
-	fmt.Println("Starting server on port 8080")
+	fmt.Printf("Domi")
 
-	r := routes.SetupRouter()
-	r.Run()
+	hook, _ := github.New(github.Options.Secret("MyGitHubSuperSecretSecrect...?")) //We need to pull the secret from the env
+
+	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		payload, err := hook.Parse(r, github.ReleaseEvent, github.PullRequestEvent)
+		if err != nil {
+			if err == github.ErrEventNotFound {
+				// ok event wasn;t one of the ones asked to be parsed
+			}
+		}
+		switch payload.(type) {
+
+		case github.ReleasePayload:
+			release := payload.(github.ReleasePayload)
+			// Do whatever you want from here...
+			fmt.Printf("%+v", release)
+
+		case github.PullRequestPayload:
+			pullRequest := payload.(github.PullRequestPayload)
+			// Do whatever you want from here...
+			fmt.Printf("%+v", pullRequest)
+		}
+	})
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
