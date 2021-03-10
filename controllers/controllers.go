e387d730891be4ee931519baab161f2d2406bd65
Julio Jimenez <julio@julioj.com>
2021-03-10 10:25:20 -0500

    app_id

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -42,65 +42,61 @@ func ReceiveGitHubWebHook(c *gin.Context) {
 	
 	switch payload.(type) {
 	case github.PushPayload:
-		cCopy := c.Copy()
-		go func() {
-			fmt.Println("ok")
-			push := payload.(github.PushPayload)
-			githubProvider.InstallationID = int64(push.Installation.ID)
-			owner := push.Repository.Owner.Login
-			repo := push.Repository.Name
-			sha := push.After
-			githubClient, err := githubProvider.GitHubAuthenticator()
-			if err != nil {
-				log.Println(errors.New("GitHub Provider Authentication Failed"))
+		push := payload.(github.PushPayload)
+		githubProvider.InstallationID = int64(push.Installation.ID)
+		owner := push.Repository.Owner.Login
+		repo := push.Repository.Name
+		sha := push.After
+		githubClient, err := githubProvider.GitHubAuthenticator()
+		if err != nil {
+			log.Println(errors.New("GitHub Provider Authentication Failed"))
+		}
+		log.Println("GitHub Provider Authentication Succeeded")
+		archiveLink, _, err := githubClient.Repositories.GetArchiveLink(c, owner, repo, "zipball", &ghclient.RepositoryContentGetOptions{Ref: sha}, true)
+		if err != nil {
+			log.Println(err)
+		}
+		archiveURL := archiveLink.String()
+		fs := lib.OSFS{}
+		domiID, err := lib.DownloadFile(fs, archiveURL)
+		if err != nil {
+			log.Println(err)
+		}
+		unzipErr := lib.UnZip(fmt.Sprintf("/tmp/%s.zip", domiID), fmt.Sprintf("/tmp/%s", domiID))
+		if unzipErr != nil {
+			log.Println(err)
+		}
+		foundFiles, e := lib.FindFiles(fs, fmt.Sprintf("/tmp/%s", domiID), ".*\\.(tf|yaml|yml)")
+		if e != nil {
+			log.Println(e)
+		}
+		detailsURL := "https://example.com"
+		// status := "queued"
+		conclusion := "neutral"
+		// startedAt := time.Now()
+		// title := "domi - Policy-as-Code Enforcer"
+		// summary := "Please stand by while we scan your repository... :thumbs-up:"
+		// text := "Something can go here"
+		if len(foundFiles) > 0 {
+			_, _, checkError := githubClient.Checks.CreateCheckRun(c, owner, repo, ghclient.CreateCheckRunOptions{
+				Name:	"domi",
+				HeadSHA: sha,
+				DetailsURL: &detailsURL,
+				// Status: &status,
+				Conclusion: &conclusion,
+				// StartedAt: &ghclient.Timestamp{
+				// 	Time: startedAt,
+				// },
+				// Output: &ghclient.CheckRunOutput{
+				// 	Title: &title,
+				// 	Summary: &summary,
+				// 	Text: &text,
+				// },
+			})
+			if checkError != nil {
+				log.Println(checkError)
 			}
-			log.Println("GitHub Provider Authentication Succeeded")
-			archiveLink, _, err := githubClient.Repositories.GetArchiveLink(cCopy, owner, repo, "zipball", &ghclient.RepositoryContentGetOptions{Ref: sha}, true)
-			if err != nil {
-				log.Println(err)
-			}
-			archiveURL := archiveLink.String()
-			fs := lib.OSFS{}
-			domiID, err := lib.DownloadFile(fs, archiveURL)
-			if err != nil {
-				log.Println(err)
-			}
-			unzipErr := lib.UnZip(fmt.Sprintf("/tmp/%s.zip", domiID), fmt.Sprintf("/tmp/%s", domiID))
-			if unzipErr != nil {
-				log.Println(err)
-			}
-			foundFiles, e := lib.FindFiles(fs, fmt.Sprintf("/tmp/%s", domiID), ".*\\.(tf|yaml|yml)")
-			if e != nil {
-				log.Println(e)
-			}
-			detailsURL := "https://example.com"
-			// status := "queued"
-			conclusion := "neutral"
-			// startedAt := time.Now()
-			// title := "domi - Policy-as-Code Enforcer"
-			// summary := "Please stand by while we scan your repository... :thumbs-up:"
-			// text := "Something can go here"
-			if len(foundFiles) > 0 {
-				_, _, checkError := githubClient.Checks.CreateCheckRun(cCopy, owner, repo, ghclient.CreateCheckRunOptions{
-					Name:	"domi",
-					HeadSHA: sha,
-					DetailsURL: &detailsURL,
-					// Status: &status,
-					Conclusion: &conclusion,
-					// StartedAt: &ghclient.Timestamp{
-					// 	Time: startedAt,
-					// },
-					// Output: &ghclient.CheckRunOutput{
-					// 	Title: &title,
-					// 	Summary: &summary,
-					// 	Text: &text,
-					// },
-				})
-				if checkError != nil {
-					log.Println(checkError)
-				}
-			}
-		}()
+		}
 		c.String(http.StatusOK, "Payload Received")
 	default:
 		c.String(http.StatusNotImplemented, "Event Not Implemented")
