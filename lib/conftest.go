b19fe81aa6569fe0534fb61cd35108b81e33d420
Julio Jimenez <julio@julioj.com>
2021-03-15 23:20:37 -0400

    feat: conftest

--- lib/conftest.go
+++ lib/conftest.go
@@ -1,47 +1,44 @@
 package lib
 
 import (
-	"os/exec"
-
-	"github.com/zclconf/go-cty/cty/msgpack"
-	"google.golang.org/grpc/metadata"
+	// "os/exec"
 )
 
-// ConftestResults - Holds conftest results
-type ConftestResults []struct {
-	filename 	string	`json:"filename"`
-	successes	int		`json:"successes,omitempty"`
-	failures	[]struct {
-		msg			string	`json:"msg"`
-		metadata	struct {
-			details	struct {
+// // ConftestResults - Holds conftest results
+// type ConftestResults []struct {
+// 	filename 	string	`json:"filename"`
+// 	successes	int		`json:"successes,omitempty"`
+// 	failures	[]struct {
+// 		msg			string	`json:"msg"`
+// 		metadata	struct {
+// 			details	struct {
 
-			} `json:"metadata,omitempty"`
-		} `json:"failures,omitempty"`
-	}
-}
+// 			} `json:"metadata,omitempty"`
+// 		} `json:"failures,omitempty"`
+// 	}
+// }
 
 // Scan - Use conftest to Scan discovered files.
-func Scan(policyPaths []string, files []string) (ConftestResults, error) {
-	fs := OSFS{}
-	policyRepoFile, policyRepoError := DownloadFile(fs, policyRepo)
-	conftestExec, lookpathErr := exec.LookPath("conftest")
-	if lookpathErr != nil {
-		return nil, lookpathErr
-	}
+func Scan(policyPaths []string, files []string) {
+	// fs := OSFS{}
+	// policyRepoFile, policyRepoError := DownloadFile(fs, policyRepo)
+	// conftestExec, lookpathErr := exec.LookPath("conftest")
+	// if lookpathErr != nil {
+	// 	return nil, lookpathErr
+	// }
 	var policyPathArguments []string
 	for _, policyPath := range policyPaths {
 		policyPathArguments = append(policyPathArguments, "-p")
 		policyPathArguments = append(policyPathArguments, policyPath)
 	}
-	arguments := []string{conftestExec, "test", "--all-namespaces", "-o", "json"}
-	arguments = append(arguments, policyPathArguments...)
-	arguments = append(arguments, files...)
-	for _, file := range files {
-		cmd := &exec.Cmd{
-			Path:	conftestExec,
-			Args:	[]string{conftestExec, "test", "--all-namespaces", "-o", "json"}
-		}
-	}
+	// arguments := []string{conftestExec, "test", "--all-namespaces", "-o", "json"}
+	// arguments = append(arguments, policyPathArguments...)
+	// arguments = append(arguments, files...)
+	// for _, file := range files {
+	// 	cmd := &exec.Cmd{
+	// 		Path:	conftestExec,
+	// 		Args:	[]string{conftestExec, "test", "--all-namespaces", "-o", "json"},
+	// 	}
+	// }
 	
 }
