9e4a085ef3799d7c2a16224b443f930821ed0373
Julio Jimenez <julio@julioj.com>
2021-03-20 12:05:37 -0400

    summary results

--- lib/summary.go
+++ lib/summary.go
@@ -21,7 +21,7 @@ func SummaryBuilder(conftestResults ConftestResults) (string, string) {
 	if len(conftestResults) > 0 {
 		for _, result := range conftestResults {
 			if len(result.Failures) > 0 || len(result.Warnings) > 0 {
-				summaryResultsByFile += fmt.Sprintf("**%s**\n", strings.Split(result.Filename, "/")[4:])
+				summaryResultsByFile += fmt.Sprintf("**%s**\n", strings.Join(strings.Split(result.Filename, "/")[4:], "/"))
 			}
 		}
 		conclusion = "failure"
