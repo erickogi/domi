f8fba597600dc5a91eb295e28e419567a32fabce
Julio Jimenez <julio@julioj.com>
2021-03-20 14:17:38 -0400

    summary results

--- lib/summary.go
+++ lib/summary.go
@@ -4,8 +4,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 )
+
+func rowBuilder(message struct{Msg string "json:\"msg\""; Metadata struct{details struct{}} "json:\"metadata,omitempty\""}, level string) string {
+	policyIDRegex := regexp.MustCompile("DOMI-[A-Z]*-[0-9]{3}")
+	policyID := string(policyIDRegex.Find([]byte(message.Msg)))
+	failureMessage := strings.Join(strings.Split(message.Msg, ": ")[1:], ": ") 
+	return fmt.Sprintf("| %s | %s | %s |\n", policyID, level, failureMessage)
+}
 
 // SummaryBuilder - Builds the summary for the check run.
 func SummaryBuilder(conftestResults ConftestResults) (string, string) {
@@ -22,6 +30,17 @@ func SummaryBuilder(conftestResults ConftestResults) (string, string) {
 		for _, result := range conftestResults {
 			if len(result.Failures) > 0 || len(result.Warnings) > 0 {
 				summaryResultsByFile += fmt.Sprintf("**%s**\n", strings.Join(strings.Split(result.Filename, "/")[4:], "/"))
+				summaryResultsByFile += "| Policy | Level | Description |\n| ------ | ----- | ----------- |\n"
+				if len(result.Failures) > 0 {
+					for _, failure := range result.Failures { 
+						summaryResultsByFile += rowBuilder(failure, "Deny")
+					}
+				}
+				if len(result.Warnings) > 0 {
+					for _, warning := range result.Warnings { 
+						summaryResultsByFile += rowBuilder(warning, "Warn")
+					}
+				}
 			}
 		}
 		conclusion = "failure"
