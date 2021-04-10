0315abe64608871a09a62cb5d551fed34a1b92bf
Julio Jimenez <julio@julioj.com>
2021-04-09 23:36:48 -0400

    github things

--- lib/github.go
+++ lib/github.go
@@ -1,7 +1,6 @@
 package lib
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"log"
@@ -11,7 +10,6 @@ import (
 
 	"github.com/bradleyfalzon/ghinstallation"
 	"github.com/google/go-github/v33/github"
-	"golang.org/x/oauth2"
 )
 
 // GitHubProvider - Contains the components necessary for GitHub
@@ -36,14 +34,11 @@ type GitHub interface {
 func NewGitHubProvider() (*GitHubProvider, error) {
 	gitHubProvider := GitHubProvider{}
 	ghPrivateKey := os.Getenv("GITHUB_PRIVATE_KEY")
-	ghToken := os.Getenv("GITHUB_ACCESS_TOKEN")
 	ghAppID, _ := strconv.Atoi(os.Getenv("GITHUB_APP_ID"))
 	gitHubProvider.GithubWebhookSecret = os.Getenv("GITHUB_WEBHOOK_SECRET")
 	if ghPrivateKey != "" {
 		gitHubProvider.githubPrivateKey = ghPrivateKey
 		gitHubProvider.AppID = ghAppID
-	} else if ghToken != "" {
-		gitHubProvider.githubToken = ghToken
 	} else {
 		return nil, errors.New("domi has no way to authenticate into GitHub")
 	}
@@ -52,39 +47,30 @@ func NewGitHubProvider() (*GitHubProvider, error) {
 
 // GitHubAuthenticator - Authenticates Personal Access Token
 func (githubProvider *GitHubProvider) GitHubAuthenticator() (*github.Client, error) {
-	if githubProvider.githubPrivateKey != "" {
-		transport := http.DefaultTransport
-		pemFile, err := os.Create("/domi/private-key.pem")
-		if err != nil {
-			log.Println(err)
-			return nil, errors.New("Error creating PEM file")
-		}
-		bytesWritten, err := pemFile.WriteString(githubProvider.githubPrivateKey)
-		if err != nil {
-			log.Println(err)
-			pemFile.Close()
-			return nil, errors.New("Error writing to PEM file")
-		}
-		fmt.Println(bytesWritten, "bytes written successfully to PEM File")
-		err = pemFile.Close()
-		if err != nil {
-			fmt.Println(err)
-			return nil, errors.New("Error closing PEM file")
-		}
-		itr, err := ghinstallation.NewKeyFromFile(transport, int64(githubProvider.AppID), int64(githubProvider.InstallationID), "/domi/private-key.pem")
-		if err != nil {
-			log.Println(err)
-		}
-		githubProvider.installationTransport = itr
-		githubProvider.appClient = &http.Client{Transport: itr}
-		githubProvider.githubClient = github.NewClient(githubProvider.appClient)
-		return githubProvider.githubClient, nil
+	transport := http.DefaultTransport
+	pemFile, err := os.Create("/domi/private-key.pem")
+	if err != nil {
+		log.Println(err)
+		return nil, errors.New("Error creating PEM file")
 	}
-	ctx := context.Background()
-	tokenSource := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: githubProvider.githubToken},
-	)
-	githubProvider.oauthClient = oauth2.NewClient(ctx, tokenSource)
-	githubProvider.githubClient = github.NewClient(githubProvider.oauthClient)
+	bytesWritten, err := pemFile.WriteString(githubProvider.githubPrivateKey)
+	if err != nil {
+		log.Println(err)
+		pemFile.Close()
+		return nil, errors.New("Error writing to PEM file")
+	}
+	fmt.Println(bytesWritten, "bytes written successfully to PEM File")
+	err = pemFile.Close()
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Error closing PEM file")
+	}
+	itr, err := ghinstallation.NewKeyFromFile(transport, int64(githubProvider.AppID), int64(githubProvider.InstallationID), "/domi/private-key.pem")
+	if err != nil {
+		log.Println(err)
+	}
+	githubProvider.installationTransport = itr
+	githubProvider.appClient = &http.Client{Transport: itr}
+	githubProvider.githubClient = github.NewClient(githubProvider.appClient)
 	return githubProvider.githubClient, nil
 }
--- lib/github_test.go
+++ lib/github_test.go
@@ -6,20 +6,17 @@ import (
 )
 
 func TestNewGitHubProvider(t *testing.T) {
-	os.Setenv("GITHUB_ACCESS_TOKEN", "okay")
 	os.Setenv("GITHUB_APP_ID", "1")
 	os.Setenv("GITHUB_WEBHOOK_SECRET", "okay")
 	_, err1 := NewGitHubProvider()
-	if err1 != nil {
+	if err1 == nil {
 		t.Fail()
 	}
-	os.Setenv("GITHUB_ACCESS_TOKEN", "")
 	os.Setenv("GITHUB_PRIVATE_KEY", "okay")
 	_, err2 := NewGitHubProvider()
 	if err2 != nil {
 		t.Fail()
 	}
-	os.Setenv("GITHUB_ACCESS_TOKEN", "")
 	os.Setenv("GITHUB_PRIVATE_KEY", "")
 }
 
