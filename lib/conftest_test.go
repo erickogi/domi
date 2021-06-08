1d6fac7440ce19fd5ffa4ebbc58e14f4f0f82d92
Julio Jimenez <julio@julioj.com>
2021-06-08 10:24:00 -0400

    feat: Get Installation ID from Check Run Response.

--- lib/conftest_test.go
+++ lib/conftest_test.go
@@ -7,7 +7,7 @@ import (
 func TestScan(t *testing.T) {
 	fs := OSFS{}
 	result := Scan(fs, "../__testdata__", "test-policy", []string{"../__testdata__/test.tf"})
-	if result != nil {
+	if result == nil {
 		t.Error()
 	}
 }
